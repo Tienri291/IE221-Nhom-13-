2cc321bd1fede4af6bc52f51129161c076662396
Nguyen Thi Cam Tien <93026517+Tienri291@users.noreply.github.com>
2022-05-13 00:04:30 +0700

    .

--- IE221Group13/settings.py
+++ IE221Group13/settings.py
@@ -128,6 +128,11 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 
 
 # Default primary key field type
